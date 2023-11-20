403a46094150edcdc38ca405349361764d339a7c
Artem Aniskin <artemaistrike@gmail.com>
2023-11-20 19:39:25 +0200

    delete export fontsize from layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
   description: "Easy Drumpad – fast and simple",
 };
 
-export const fontSans = FontSans({
+const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
 });

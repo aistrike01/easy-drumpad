921129426f2281a9851562e2475cd39f63065add
Artem Aniskin <artemaistrike@gmail.com>
2024-02-03 00:07:27 +0200

    feature: add prettier, format all code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 export default function Home() {
   return (
-    <main className="bg-green-600 h-screen">
-      <h1 className="flex flex-wrap gap-12 items-center justify-center h-full text-center text-6xl font-bold  text-black">
+    <main className="h-screen bg-green-600">
+      <h1 className="flex h-full flex-wrap items-center justify-center gap-12 text-center text-6xl font-bold  text-black">
         {Array.from({ length: 16 }).map((pad, i) => (
           <div
             key={i}
-            className="h-32 cursor-pointer hover:scale-95 hover:bg-orange-400 w-32 brad rounded-md shadow-inner bg-gray-400"
+            className="brad h-32 w-32 cursor-pointer rounded-md bg-gray-400 shadow-inner hover:scale-95 hover:bg-orange-400"
           ></div>
         ))}
       </h1>
